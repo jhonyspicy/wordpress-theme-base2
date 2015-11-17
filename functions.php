9c81a6f65088854e22d4b0f49142037e81304aa0
takara <takara@sal.ne.jp>
2015-11-17 15:47:55 +0900

    パンくずのnamespaceの修正。

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 \Jhonyspicy\Wordpress\Theme\Base\Base::initialize(); //テーマベース
-\Atomita\Wordpress\BreadcrumbNavigationFacade::expandFunction(); //パンくず
+\atomita\wordpress\BreadcrumbNavigationFacade::expandFunction(); //パンくず
 
 Theme::add_hooks();
 Thumbnail::add_hooks();
