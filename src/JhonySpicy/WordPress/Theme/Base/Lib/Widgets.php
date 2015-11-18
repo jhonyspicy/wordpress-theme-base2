683901a37b6ac05c52dcb67d01cf587ccd77d3f3
takara <takara@sal.ne.jp>
2015-11-18 15:27:45 +0900

    ウィジェットの管理画面で必要になるスタイルやスクリプトを読み込む処理を修正

--- src/JhonySpicy/WordPress/Theme/Base/Lib/Widgets.php
+++ src/JhonySpicy/WordPress/Theme/Base/Lib/Widgets.php
@@ -75,10 +75,15 @@ abstract class Widgets {
 
 		foreach(self::$widgetList as $widget) {
 			$widgetName = self::name($widget);
-			$file_path = '/scripts/admin/widgets/'. $widgetName .'.js';
 
-			if (is_file(get_template_directory() . $file_path)) {
-				wp_enqueue_script($widgetName . '_script', get_template_directory_uri() . $file_path, array('jquery'), '1.0.0', true);
+			if (method_exists($widget, 'admin_print_scripts')) {
+				call_user_func(array($widget, 'admin_print_scripts'));
+			} else {
+				$file_path = '/scripts/admin/widgets/'. $widgetName .'.js';
+
+				if (is_file(get_template_directory() . $file_path)) {
+					wp_enqueue_script($widgetName . '_script', get_template_directory_uri() . $file_path, array('jquery'), '1.0.0', true);
+				}
 			}
 		}
 	}
@@ -86,10 +91,15 @@ abstract class Widgets {
 	static public function admin_print_styles() {
 		foreach(self::$widgetList as $widget) {
 			$widgetName = self::name($widget);
-			$file_path = '/styles/admin/widgets/'. $widgetName .'/style.css';
 
-			if (is_file(get_template_directory() . $file_path)) {
-				wp_enqueue_style($widgetName, get_template_directory_uri() . $file_path, array(), '1.0.0');
+			if (method_exists($widget, 'admin_print_styles')) {
+				call_user_func(array($widget, 'admin_print_styles'));
+			} else {
+				$file_path = '/styles/admin/widgets/'. $widgetName .'/style.css';
+
+				if (is_file(get_template_directory() . $file_path)) {
+					wp_enqueue_style($widgetName, get_template_directory_uri() . $file_path, array(), '1.0.0');
+				}
 			}
 		}
 	}
